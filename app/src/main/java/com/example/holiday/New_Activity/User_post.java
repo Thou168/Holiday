6f32b30034043bc62d6b72a036ab711d671c4f44
Thou <Senthou168@gmail.com>
2019-06-13 11:22:21 +0700

    first commit1

--- app/src/main/java/com/example/holiday/New_Activity/User_post.java
+++ app/src/main/java/com/example/holiday/New_Activity/User_post.java
@@ -27,7 +27,6 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class User_post extends AppCompatActivity {
 
     Window window;
-    ArrayList<Item_Post>items;
     private static ViewPager viewPager;
     private static TabLayout tabLayout;
     private PagerAdapter pagerAdapter;
@@ -35,54 +34,55 @@ public class User_post extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_post);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        findViewById(R.id.tv_back).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(getResources().getColor(R.color.colorMessage));
-
-        CircleImageView cv = (CircleImageView) findViewById(R.id.img_user);
-        cv.setImageResource(getIntent().getIntExtra("img_user",0));
-        TextView tv_name = (TextView) findViewById(R.id.tv_name);
-        tv_name.setText(getIntent().getStringExtra("name"));
-//        items = new ArrayList<Item_Post>();
-//        items = (ArrayList<Item_Post>)getIntent().getSerializableExtra("items");
-//        int id = getIntent().getIntExtra("id",0);
-//
-//        ArrayList<Item_Post> item = new ArrayList<>();
-//        for(int i = 0;i<items.size();i++){
-//            if(items.get(i).getId() == id){
-//                item.add(items.get(i));
+//        Toolbar toolbar = findViewById(R.id.toolba
+////        setSupportActionBar(toolbar);
+//        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
 //            }
-//        }
-
-//        LinearLayoutManager layoutManager1 = new LinearLayoutManager(getBaseContext(), LinearLayoutManager.VERTICAL, false);
-//        Vertical_adapter adapter1 = new Vertical_adapter(getBaseContext(),item);
-//        RecyclerView recy_horizontal1 = (RecyclerView)findViewById(R.id.recyclerview);
-//        recy_horizontal1.setHasFixedSize(true);
-//        recy_horizontal1.setLayoutManager(layoutManager1);
-//        recy_horizontal1.setAdapter(adapter1);
-        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(pagerAdapter);
-        tabLayout = (TabLayout) findViewById(R.id.tab);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
-
+//        });
+//        findViewById(R.id.tv_back).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+//
+//        window = getWindow();
+//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//        window.setStatusBarColor(getResources().getColor(R.color.colorMessage));
+//
+//        CircleImageView cv = (CircleImageView) findViewById(R.id.img_user);
+//        cv.setImageResource(getIntent().getIntExtra("img_user",0));
+//        TextView tv_name = (TextView) findViewById(R.id.tv_name);
+//        tv_name.setText(getIntent().getStringExtra("name"));
+////        items = new ArrayList<Item_Post>();
+////        items = (ArrayList<Item_Post>)getIntent().getSerializableExtra("items");
+////        int id = getIntent().getIntExtra("id",0);
+////
+////        ArrayList<Item_Post> item = new ArrayList<>();
+////        for(int i = 0;i<items.size();i++){
+////            if(items.get(i).getId() == id){
+////                item.add(items.get(i));
+////            }
+////        }
+//
+////        LinearLayoutManager layoutManager1 = new LinearLayoutManager(getBaseContext(), LinearLayoutManager.VERTICAL, false);
+////        Vertical_adapter adapter1 = new Vertical_adapter(getBaseContext(),item);
+////        RecyclerView recy_horizontal1 = (RecyclerView)findViewById(R.id.recyclerview);
+////        recy_horizontal1.setHasFixedSize(true);
+////        recy_horizontal1.setLayoutManager(layoutManager1);
+////        recy_horizontal1.setAdapter(adapter1);
+//        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+//        viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        viewPager.setAdapter(pagerAdapter);
+    ArrayList<Item_Post>items;
+//        tabLayout = (TabLayout) findViewById(R.id.tab);
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+//        r);
 
     }
     public class PagerAdapter extends FragmentPagerAdapter {
